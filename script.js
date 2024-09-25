2ad08c4a40daa7318df0ebe66ef0b2ceb36cddec
Md Sujon Mia <mia258549@gmail.com>
2024-09-25 15:12:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const toggleOnButton = document.getElementById('navOpen');
+const toggleOffButton = document.getElementById('navOff');
+const navbarLinks = document.getElementById('navbarLinks');
+
+// Show the navbar
+toggleOnButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active'); // Show the navbar
+});
+
+
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
